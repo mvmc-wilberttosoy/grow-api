92d25a4f4f88068326cbbc4c02fdfd5b44ac9a53
mvmc-wilberttosoy <wtosoy@mvmc.com.ph>
2026-02-18 08:42:07 +0800

    uniform model added

---
+++ models/Uniform.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const uniformSchema = new mongoose.Schema(
+    {
+        employeeId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        isOnsiteChoosing: {
+            type: Boolean,
+            default: false,
+        },
+        topSize: {
+            type: String,
+        },
+        bottomSize: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('Uniform', uniformSchema);
