63f779d7f4de0dcd330976486b30a6e7fbdf15e7
mvmc-wilberttosoy <wtosoy@mvmc.com.ph>
2026-02-18 08:20:37 +0800

    user model added

---
+++ models/User.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        middleName: {
+            type: String,
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        birthday: {
+            type: Date,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        isDefaultPassword: {
+            type: Boolean,
+            required: true
+        },
+        contactNumber: {
+            type: String,
+            required: true
+        },
+        divisionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Division',
+            required: true
+        },
+        departmentId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Department',
+            required: true
+        },
+        position: {
+            type: String,
+            required: true,
+        },
+        startDate: {
+            type: Date,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+            default: 'Active'
+        },
+        role: {
+            type: String,
+            required: true,
+            enum: ['User', 'HR', 'Admin'],
+            default: 'User'
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('User', userSchema);
