d14e4863ca4883573240c0734a29eba33ba8ac3f
mvmc-wilberttosoy <wtosoy@mvmc.com.ph>
2026-02-18 09:58:42 +0800

    companyId model added

---
+++ models/CompanyId.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const companyIdSchema = new mongoose.Schema(
+    {
+        employeeId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        picture: {
+            url: {
+                type: String
+            },
+            publicId: {
+                type: String
+            }
+        },
+        emergencyContactName: {
+            type: String,
+        },
+        emergencyContactAddress: {
+            type: String,
+        },
+        emergencyContactNumber: {
+            type: String,
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('CompanyId', companyIdSchema);
