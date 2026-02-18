fad14a534a8945b6acb06fd89646d50ec75a9cd0
mvmc-wilberttosoy <wtosoy@mvmc.com.ph>
2026-02-18 15:38:56 +0800

    division controller done

--- source/controllers/division.controller.js
+++ source/controllers/division.controller.js
@@ -70,8 +70,75 @@ const getDivisionById = async (req, res) => {
     }
 }
 
+// Update division by Id
+const updateDivisionById = async (req, res) => {
+    try {
+        const { divisionId } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(divisionId)) {
+            return res.status(400).json({ message: 'Invalid division ID format' });
+        }
+
+        const division = await Division.findById(divisionId);
+
+        if (!division) {
+            return res.status(404).json({ message: 'Division not found' });
+        }
+
+        const { name, description } = req.body;
+
+        if (!name) {
+            return res.status(400).json({ message: 'Division name is required' });
+        }
+
+        const updatedDivision = await Division.findByIdAndUpdate(
+            divisionId,
+            {
+                name,
+                description
+            },
+            {
+                returnDocument: 'after'
+            }
+        )
+
+        if (!updatedDivision) {
+            return res.status(404).json({ message: 'Division not found' });
+        }
+
+        return res.status(200).json(updatedDivision);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Something went wrong' });
+    }
+}
+
+
+const deleteDivisionById = async (req, res) => {
+    try {
+        const { divisionId } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(divisionId)) {
+            return res.status(400).json({ message: 'Invalid division ID format' });
+        }
+
+        const deletedDivision = await Division.findByIdAndDelete(divisionId);
+
+        if (!deletedDivision) {
+            return res.status(404).json({ message: 'Division not found' });
+        }
+
+        return res.status(200).json({ message: 'Division deleted successfully' });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Something went wrong' });
+    }
+}
+
 module.exports = {
     createNewDivision,
     getDivisions,
-    getDivisionById
+    getDivisionById,
+    updateDivisionById,
+    deleteDivisionById
 };
